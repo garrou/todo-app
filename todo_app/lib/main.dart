28c169bc46fc53fa16e2c48f83e54350a8c8ec1e
Adrien Garrouste <garrousteadrien@gmail.com>
2021-01-29 17:53:19 +0100

    Add post in database

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -2,12 +2,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app/login.dart';
 
-Future<void> main() async {
-  await Firebase.initializeApp();
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
